86329b0dd141eb498412c3d3b5292377108fbff8
crenshaw-picklops <jesse.desmarais@ucalgary.ca>
2024-03-07 21:52:03 -0700

    Deleted replaced code to increase clarity.

--- Replication/spoofy_rep_receive.py
+++ Replication/spoofy_rep_receive.py
@@ -4,26 +4,21 @@ import sys
 from threading import Thread, Lock
 from queue import Queue
 
-PORT_NUM = 9001
 LISTENFOR = 10000
 LOCALHOST = "127.0.0.1"
 
 def server_program():
 
     in_queue = Queue()
-    #lock = Lock()
     db_conns = establish_connection()
-    #cursor = db.cursor()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Server socket open")
-    #ip = '127.0.0.1'
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname + ".local")
     print(f"Local IP addr is: {ip}")
 
-    port = LISTENFOR # port numbers must be greater than 1000
-    server_socket.bind((ip, port))
+    server_socket.bind((ip, LISTENFOR))
     print("Server binded")
     server_socket.listen(5) # argument indicates how many clients you want to be able to communicate w/ at the same time
     print("Server listening")
@@ -34,15 +29,10 @@ def server_program():
     while True:
 
         conn, addr = server_socket.accept()
-        #acceptance = input('1')
-        #conn.send(acceptance.encode())
         print(f'Received connection from {addr}:')
 
         u_thread = Thread(target=new_request_handler,args=(conn, in_queue))
-        #u_thread = Thread(target=new_request_handler,args=(conn, db, cursor, lock, in_queue)
         u_thread.start()
-
-        #conn.close()
 
 def establish_connection():
 
@@ -63,7 +53,6 @@ def establish_connection():
 
     return db_pool
 
-#def atomic_database(db, cursor, lock, in_queue):
 def atomic_database(in_queue, pool):
 
     while True:
@@ -75,7 +64,6 @@ def atomic_database(in_queue, pool):
             cursor = db.cursor()
 
             try:
-                #with lock:
                 cursor.execute(data_item)
                 db.commit()
                 print("Database update complete\n")
@@ -85,7 +73,6 @@ def atomic_database(in_queue, pool):
             cursor.close()
             db.close()
 
-#def new_request_handler(con, db, cursor, lock, in_queue):
 def new_request_handler(con, in_queue):
     recv_msg = ""
 
@@ -102,8 +89,6 @@ def new_request_handler(con, in_queue):
         #     recv_msg = ""
         #     atomic_database(db, cursor, lock, in_queue)
         # recv_msg += rtemp
-
-
 
 if __name__ == "__main__":
     server_program()
