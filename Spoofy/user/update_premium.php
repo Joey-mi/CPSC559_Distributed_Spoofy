d126865a016285a00cacac79abf109514874c2c5
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-04-09 15:24:33 -0600

    Cleanup

--- Spoofy/user/update_premium.php
+++ Spoofy/user/update_premium.php
@@ -7,41 +7,18 @@ if (isset($_SESSION["LoggedIn"]) && $_SESSION["LoggedIn"]) {
     $Premium = filter_var($_GET["Premium"], FILTER_VALIDATE_BOOLEAN);
     $UserID = $_SESSION["UserID"];
 
-    		// 	// Replace single quotes with escape characters
-			// $name = str_replace("'", "\'", $name);
-			// $about = str_replace("'", "\'", $about);
-			// $pfp = str_replace("'", "\'", $pfp);
-			// $bp = str_replace("'", "\'", $bp);
-
-			// $sql = "UPDATE ARTIST SET Name='$name', About='$about', ProfilePicture='$pfp', BannerPicture='$bp' WHERE ArtistID=$ArtistID";
-			// sendQuery($sql);
-			// header("Refresh:0; url=manage_artists.php"); // Test for refreshing page after update
-
-            
     if ($Premium) {
         $bool = TRUE;
-        // $prepare = mysqli_prepare($con, $sql);
-        // if ($prepare) {
-            $renew_date = date("Y-m-d", strtotime("+1 month"));
-            echo $renew_date;
-            // $prepare -> bind_param("ss", '$renew_date', $UserID);
-            // $prepare -> execute();
-            $_SESSION["IsPremium"] = true;
-            $sql = "UPDATE USER SET IsPremium='$bool', SubRenewDate='$renew_date' WHERE UserID='$UserID'";
-            sendQuery($sql);
-        // }
-        // $prepare -> close();
+        $renew_date = date("Y-m-d", strtotime("+1 month"));
+        $_SESSION["IsPremium"] = true;
+        $sql = "UPDATE USER SET IsPremium='$bool', SubRenewDate='$renew_date' WHERE UserID='$UserID'";
+        echo $sql;
     } else {
         $bool = FALSE;
-        // $prepare = mysqli_prepare($con, $sql);
-        // if ($prepare) {
-        //     $prepare -> bind_param("s", $UserID);
-            // $prepare -> execute();
-            $_SESSION["IsPremium"] = false;
-            $sql = "UPDATE USER SET IsPremium='$bool', SubRenewDate=NULL WHERE UserID='$UserID'";
-            sendQuery($sql);
-        // }
-        // $prepare -> close();
+        $_SESSION["IsPremium"] = false;
+        $sql = "UPDATE USER SET IsPremium='$bool', SubRenewDate=NULL WHERE UserID='$UserID'";
+        echo $sql;
+        sendQuery($sql);
     }
 
     // if ($Premium) {
