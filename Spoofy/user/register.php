16f5e48a78b26bb0d40341300c68ef8bc2f51800
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-04-09 12:59:41 -0600

    Register update

--- Spoofy/user/register.php
+++ Spoofy/user/register.php
@@ -65,13 +65,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     if(empty($error_string)) {
         
         // Prepare an insert statement
-        $sql = "INSERT INTO USER (Username, PasswordHash, IsPremium) VALUES ('$username', '$password_hash', FALSE)";
+        $sql = "INSERT INTO USER (Username, PasswordHash, IsPremium) VALUES ('$username', '$password_hash', 'FALSE')";
         sendQuery($sql);
         header("Refresh:0; url=/user/login.php");
     }
-    
-    // Close connection
-    mysqli_close($con);
 }
 ?>
 
