ee3b586b26bb61b10f92d0fc61cb6adb0c7e66a2
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-04-09 15:57:47 -0600

    Merged distributor branch

--- Spoofy/user/update_premium.php
+++ Spoofy/user/update_premium.php
@@ -8,17 +8,32 @@ if (isset($_SESSION["LoggedIn"]) && $_SESSION["LoggedIn"]) {
     $UserID = $_SESSION["UserID"];
 
     if ($Premium) {
-        $sql = "UPDATE USER SET IsPremium=TRUE, SubRenewDate=? WHERE UserID=$UserID";
+        $bool = TRUE;
+        $renew_date = date("Y-m-d", strtotime("+1 month"));
+        $_SESSION["IsPremium"] = true;
+        $sql = "UPDATE USER SET IsPremium='$bool', SubRenewDate='$renew_date' WHERE UserID='$UserID'";
+        echo $sql;
         sendQuery($sql);
     } else {
-        $sql = "UPDATE USER SET IsPremium=FALSE, SubRenewDate=NULL WHERE UserID=$UserID";
+        $bool = FALSE;
+        $_SESSION["IsPremium"] = false;
+        $sql = "UPDATE USER SET IsPremium='$bool', SubRenewDate=NULL WHERE UserID='$UserID'";
+        echo $sql;
         sendQuery($sql);
     }
 
+    // if ($Premium) {
+    //     $sql = "UPDATE USER SET IsPremium=TRUE, SubRenewDate=$renew_date WHERE UserID=$UserID";
+    //     sendQuery($sql);
+    // } else {
+    //     $sql = "UPDATE USER SET IsPremium=FALSE, SubRenewDate=$renew_date WHERE UserID=$UserID";
+    //     sendQuery($sql);
+    // }
+
     $_SESSION["Queue"] = null;
     $_SESSION["SongIndex"] = 0;
-    header("Refresh:0; url=/user/profile.php?UserID=".$UserID);
-    // header("location: /user/profile.php?UserID=".$UserID);
+    // header("Refresh:0; url=/user/profile.php?UserID=".$UserID);
+    header("location: /user/profile.php?UserID=".$UserID);
 } else {
     // Someone not logged in is trying to update premium
     header("location: /user/login.php");
